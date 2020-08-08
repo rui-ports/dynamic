0a3a7e42b28cd7532af0356bda2283a6bdf55a61
Luoer | 罗尔 <66991791+qrovic@users.noreply.github.com>
2020-08-08 11:50:25 +0800

    Delete dynamic.sh

--- .github/workflows/dynamic.sh
+++
@@ -1,6 +0,0 @@
-cd Erfan
-sudo chmod 0777 dynamic.sh
-              sudo bash setup.sh
-              sudo ./dynamic.sh $ROM_NAME file.zip
-              sudo ./make.sh system $ROM_NAME AB out
-              sudo ./make.sh system $ROM_NAME Aonly out
