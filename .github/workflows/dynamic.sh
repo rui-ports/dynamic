433c7b6a34140fa98a0509ab8cf88770a9d75037
Luoer | 罗尔 <66991791+qrovic@users.noreply.github.com>
2020-08-08 11:28:12 +0800

    bruh

---
+++ .github/workflows/dynamic.sh
@@ -0,0 +1,6 @@
+cd Erfan
+sudo chmod 0777 dynamic.sh
+              sudo bash setup.sh
+              sudo ./dynamic.sh $ROM_NAME file.zip
+              sudo ./make.sh system $ROM_NAME AB out
+              sudo ./make.sh system $ROM_NAME Aonly out
